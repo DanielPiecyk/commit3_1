0a3580fe0326be32eaa72d622417b17c042e1bb9
danielpiecyk <100698293+DanielPiecyk@users.noreply.github.com>
2022-04-12 13:02:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int wybor = 1,wiek;
+    String imie,nazwisko;
+    try {
+      Service1 s = new Service1();
+       while(wybor!=0) 
+    {
+       System.out.print("2.Wyswietlanie\n1.Dodaj studenta\n0.zakończ program\n");
+      wybor = sc.nextInt();
+      switch(wybor)
+        {
+            case 2:
+            {/*wyswietlanie*/
+             var students = s.getStudents();
+             for(Student current : students) 
+             {
+             System.out.println(current.ToString());
+               
+            }
+              break;
+            }
+          case 1:
+            {/*dodaj studenta*/
+              System.out.print("Podaj imię :\n");
+              sc.nextLine();
+              imie = sc.nextLine();
+              System.out.print("Podaj nazwisko :\n");
+              nazwisko = sc.nextLine();
+              System.out.print("Podaj wiek :\n");
+              wiek = sc.nextInt();
+              s.addStudent(new Student(imie, wiek,nazwisko));
+              break;
+              }
+          case 0:
+            {break;}
+        }
+    }
+    
+    } catch (IOException e) {
+
+    }
+  }
+}
